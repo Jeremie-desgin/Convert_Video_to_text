a10708f497ecac278443b5a36b346782194f6198
Jeremie-desgin <jkwapong27@neiacademy.org>
2025-11-05 15:35:40 -0500

    Convert ACSII
    This is not resized

--- #ASCII.py
+++ #ASCII.py
@@ -1,23 +1,49 @@
 #ASCII
 
-import PIL
-from PIL import Image
+from PIL import Image, ImageDraw , ImageFont
+import math
 
 
-chars = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/|()1{}[]?-_+~<>i!lI;:,\"^`'. "
+#chars = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/|()1{}[]?-_+~<>i!lI;:,\"^`'. "
+chars = " .:;+*#%@"
+charArray = list(chars)
+charLength = len(charArray)
+interval =  charLength/256
 
-im = Image.open("nemo.webp") 
+
+
+scaleFactor = 0.4
+onecharWidth = 8
+onecharHeight = 18
+char_aspect_ratio = onecharWidth/onecharHeight
+
+
+def getchar(inputint):
+    return charArray[math.floor(inputint*interval)]
+
+text_file = open("output.txt", "w")
+
+im = Image.open("nemo.webp").convert("RGB")
+width, height = im.size
+
+new_width = int(scaleFactor * width * char_aspect_ratio)
+new_height = int(scaleFactor * height * char_aspect_ratio)
+
+im = im.resize((new_width,new_height))
 width, height = im.size
 pix = im.load()
 
-print(im.mode)
+#print(im.mode)
 print(f"This is WxL {width},{height}")
 
 for i in range(height):
     for j in range(width):
         r, g, b = pix[j, i]
         #print(r)
-        h = int((r+ g+ b)/3)
-        pix[j,i] = (h,h,h)
-
+        h = int((r + g + b)/3)
+        pix[j,i] = (h, h, h)
+        text_file.write(getchar(h))
+    text_file.write('\n')
+text_file.close()
 im.save ("output.png")
+print("Converted!")
